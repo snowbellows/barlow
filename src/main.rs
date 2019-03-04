f8f6b4ca7ade821bc9f1d7b0a55a871bbec64cbb
snowbellows <aleyhalverson@gmail.com>
2019-03-04 22:25:35 +1100

    diesel setup

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,89 @@
+pub mod models;
+pub mod schema;
+
 #[macro_use]
 extern crate log;
 extern crate pretty_env_logger;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate warp;
 extern crate dotenv;
+extern crate warp;
+#[macro_use]
+extern crate diesel;
 
+use self::models::*;
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
+use dotenv::dotenv;
 use std::env;
 use warp::{http::StatusCode, Filter};
-use dotenv::dotenv;
+
+type PgPool = Pool<ConnectionManager<PgConnection>>;
+type PooledPg = PooledConnection<ConnectionManager<PgConnection>>;
 
 fn main() {
     dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     if env::var_os("RUST_LOG").is_none() {
         // Set `RUST_LOG=barlow=debug` to see debug logs,
         // this only shows access logs.
         env::set_var("RUST_LOG", "barlow=info");
     }
+
     pretty_env_logger::init();
 
-    let index = warp::fs::file("static/index.html");
+    let database_pool = establish_pool(database_url);
 
-    warp::serve(index).run(([127, 0, 0, 1], 3030));
+    //Setup connection pool as Warp filter
+    let pg = warp::any()
+        .map(move || database_pool.clone())
+        .and_then(|pool: PgPool| match pool.get() {
+            Ok(conn) => Ok(conn),
+            Err(e) => Err(warp::reject::custom(e)),
+        });
+
+    let api_v1 = warp::path("api").and(warp::path("v1"));
+
+    let blog = api_v1.and(warp::path("blog"));
+    let blog_index = blog.and(warp::path::end());
+    // GET /api/blog
+    let blog_list = warp::get2()
+        .and(blog_index)
+        .and(pg)
+        .and_then(show_posts);
+
+    let api = blog_list;
+
+    let index = warp::fs::file("static/index.html").and(warp::path::end());
+
+
+    let routes = api.or(index).with(warp::log("barlow"));
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030));
+}
+
+fn establish_pool(database_url: String) -> PgPool {
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+
+    Pool::new(manager).expect("Postgres connection pool could not be created")
+}
+
+//API handlers
+
+fn show_posts(connection: PooledPg) -> Result<impl warp::Reply, warp::Rejection> {
+    use self::schema::posts::dsl::*;
+
+    let blog_posts = posts
+        .filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(&connection);
+
+    match blog_posts {
+        Ok(xs) => Ok(warp::reply::json(&xs)),
+        Err(e) => Err(warp::reject::custom(e)),
+    }
 }
